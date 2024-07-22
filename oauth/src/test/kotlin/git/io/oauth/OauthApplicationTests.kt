d3909243ff20ede3c0b3eaffc999274649d316ab
Junnyjun <chbe5082@naver.com>
2024-07-22 13:57:45 +0900

    init module oauth

---
+++ oauth/src/test/kotlin/git/io/oauth/OauthApplicationTests.kt
@@ -0,0 +1,13 @@
+package git.io.oauth
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OauthApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
