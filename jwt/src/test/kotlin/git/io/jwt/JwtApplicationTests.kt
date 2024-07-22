542500a264ad973301922fc178797aed865ae6e5
Junnyjun <chbe5082@naver.com>
2024-07-22 13:55:54 +0900

    init module

---
+++ jwt/src/test/kotlin/git/io/jwt/JwtApplicationTests.kt
@@ -0,0 +1,13 @@
+package git.io.jwt
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JwtApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
